41371faed9e495337d22ea913b45b1f7dcbe8448
Bartlomiej Pedryc <b.pedryc@poczta.fm>
2023-06-08 20:02:24 +0200

    Eliminate port 8000 from server.js and config.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ db.once('open', () => {
 });
 db.on('error', err => console.log('Error ' + err));
 
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
 
